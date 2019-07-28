ac5536657ea0b0a38143ad3d24b2fcfa8cee55d9
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-07-28 16:19:23 -0300

    fixing query from service

--- src/main/java/guru/springframework/api/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/api/services/ApiServiceImpl.java
@@ -1,7 +1,9 @@
 package guru.springframework.api.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import guru.springframework.api.domain.Post;
 
@@ -9,16 +11,21 @@ import guru.springframework.api.domain.Post;
 public class ApiServiceImpl implements ApiService {
 
 	private RestTemplate restTemplate;
+	private final String api_url;
 
-	public ApiServiceImpl(RestTemplate restTemplate) {
-		super();
+	public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
 		this.restTemplate = restTemplate;
+		this.api_url = api_url;
 	}
 
 	@Override
 	public Post getPost(Integer post_number) {
 		
-		Post post = restTemplate.getForObject("https://jsonplaceholder.typicode.com/posts/" + post_number, Post.class); 
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder
+				.fromUriString(api_url + post_number);
+				
+		System.out.println(uriBuilder.toUriString());
+		Post post = restTemplate.getForObject(uriBuilder.toUriString(), Post.class); 
 				
 		return post;
 	}
